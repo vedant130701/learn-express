3e6e38bd7678e7e641c2f8cc3d9584a7ec6ea6b9
Vedant Rishi Das <55937481+vedant130701@users.noreply.github.com>
2024-03-12 15:59:49 -0400

    modularized server4.js

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,24 @@
 const express = require('express')
 const router = express.Router();
+
+router.get('/usernames', (req, res) => {
+    let usernames = req.users.map(function(user) {
+        return {id: user.id, username: user.username};
+    });
+    res.send(usernames);
+})
+
+router.get('/username/:name', (req, res) => {
+    let uname = req.params.name;
+    let userData = req.users.filter((user) => user["username"] === uname);
+    if(userData.length != 0) {
+        res.send(userData);
+    }
+    else {
+        res.json({
+        error: {message: 'users not found', status: 404}
+    });
+    }
+})
+
+module.exports = router
